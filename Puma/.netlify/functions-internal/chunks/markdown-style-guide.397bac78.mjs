eaba20586fa845f2cc94bbaa29a0f80e8a47aece
Bill Wallbaum <bill@catsimaginganddesign.com>
2023-06-08 19:25:33 -0600

    fresh start

--- Puma/.netlify/functions-internal/chunks/markdown-style-guide.397bac78.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./markdown-style-guide.732e7070.mjs');
-					}
-					const collectedLinks = "@@ASTRO-LINKS@@";
-					const collectedStyles = "@@ASTRO-STYLES@@";
-					const collectedScripts = "@@ASTRO-SCRIPTS@@";
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
--- Puma/src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Cat Studios';
-export const SITE_DESCRIPTION = 'CatStudios.online';
