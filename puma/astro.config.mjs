77c97601d8bf51ef89a484e3e5c59c01bf56027d
Bill Wallbaum <bill@catsimaginganddesign.com>
2023-06-10 14:33:09 -0600

    adds basic config settings

--- puma/astro.config.mjs
+++ puma/astro.config.mjs
@@ -1,4 +1,20 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://catstudios.online",
+  markdown: {
+    gfm: true
+  },
+  integrations: [mdx({
+    extendMarkdownConfig: false,
+    gfm: true
+  })],
+  compressHTML: false,
+  build: {
+    // Example: Generate `page.html` instead of `page/index.html` during build.
+    format: 'file',
+    inlineStylesheets: `auto`,
+  }
+});
