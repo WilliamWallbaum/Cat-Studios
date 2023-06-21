213dd83dc7f4c80e7d6866864642d134aabe5980
Bill Wallbaum <bill@catsimaginganddesign.com>
2023-06-20 22:06:17 -0600

    corrects formatting

--- puma/src/postcss.config.js
+++ puma/src/postcss.config.js
@@ -1,16 +1,13 @@
 module.exports = {
   plugins: [
-    [
-      require("postcss-preset-env"),
-      {
-        stage: 2,
-      },
-      require("postcss-import"),
-      require("autoprefixer"),
-      require("cssnano"),
-      require("postcss-nesting"),
-      require("postcss-nested-vars"),
-      require("postcss-simple-vars"),
-    ],
+    require("postcss-preset-env")({
+      stage: 2,
+    }),
+    require("postcss-import"),
+    require("autoprefixer"),
+    require("cssnano"),
+    require("postcss-nesting"),
+    require("postcss-nested-vars"),
+    require("postcss-simple-vars"),
   ],
 };
