fea7672814a762290a58e5b3ea8f205863afaad9
Bill Wallbaum <bill@catsimaginganddesign.com>
2023-06-21 18:49:32 -0600

    removes sass plugins

--- puma/src/postcss.config.js
+++ puma/src/postcss.config.js
@@ -1,14 +1,12 @@
 module.exports = {
   plugins: [
     require("@csstools/postcss-oklab-function"),
-    require("@csstools/postcss-sass"),
     require("autoprefixer"),
     require("cssnano"),
     require("postcss-each-variables"),
     require("postcss-import"),
     require("postcss-insert"),
-    require("postcss-nested-vars"),
-    require("postcss-nesting"),
+    require("postcss-nested"),
     require("postcss-preset-env")({
       stage: 2,
     }),
