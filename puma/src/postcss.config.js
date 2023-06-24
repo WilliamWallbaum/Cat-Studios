e426b3aa4476d62b477e0b96c086e933e83d91f3
Bill Wallbaum <bill@catsimaginganddesign.com>
2023-06-23 20:48:38 -0600

    cleans up plugins and coments out code

--- puma/src/postcss.config.js
+++ puma/src/postcss.config.js
@@ -1,18 +1,14 @@
 module.exports = {
   plugins: [
-    require("@csstools/postcss-oklab-function").default, // Plugin for using the Oklab color space function in CSS
-    require("autoprefixer"), // Plugin for adding vendor prefixes to CSS rules
-    require("cssnano"), // Plugin for minifying CSS
-    require("postcss-import"), // Plugin for handling CSS imports
-    require("postcss-extend-rule"), // Plugin for extending CSS rules
-    require("postcss-advanced-variables"), // Plugin for using advanced variables in CSS
+    require("postcss-import"), // Plugin transform `@import` rules by inlining content.
+    require("postcss-nested"), // Plugin for writing nested CSS rules
+    require("postcss-extend-rule"), // lets you use the `@extend` at-rule and Functional Selectors in CSS
+    require("postcss-advanced-variables"), // $variables, @if, @else, @for, @each, @mixin, @include, and @content | https://github.com/csstools/postcss-advanced-variables
     require("postcss-preset-env")({
       stage: 2, // Specifies the stage of CSS features to enable
-    }), // Plugin for using future CSS features
-    require("postcss-atroot"), // Plugin for nesting rules inside the :root selector
-    require("postcss-property-lookup"), // Plugin for looking up and using properties defined in other rules
-    require("postcss-nested"), // Plugin for writing nested CSS rules
-    require("postcss-simple-vars"), // Plugin for using simple variables in CSS,
-    require("postcss-for"), // Plugin for using @for loops in CSS
+    }), // Plugin for using future CSS features | https://preset-env.cssdb.org/features/
+    require("autoprefixer"), // Plugin for adding vendor prefixes to CSS rules
+    require("postcss-atroot"), // The @at-root causes one or more rules to be emitted at the root of the document, rather than being nested beneath their parent selectors | https://github.com/OEvgeny/postcss-atroot
+    require("cssnano"), // Plugin for minifying CSS
   ],
 };
