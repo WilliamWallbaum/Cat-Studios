87a21715e45e04fb00cb84648de2e46fcec27a7d
Bill Wallbaum <bill@catsimaginganddesign.com>
2023-06-26 21:06:14 -0600

    removes postcss nested

--- puma/src/postcss.config.js
+++ puma/src/postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: [
     require("postcss-import"), // Plugin transform `@import` rules by inlining content. | https://github.com/postcss/postcss-import
-    require("postcss-nested"), // PostCSS plugin to unwrap nested rules closer to Sass syntax. | https://github.com/postcss/postcss-nested
+    // require("postcss-nested"), // PostCSS plugin to unwrap nested rules closer to Sass syntax. | https://github.com/postcss/postcss-nested
     // require("postcss-extend-rule"), // PostCSS Extend Rule lets you use the @extend at-rule and Functional Selectors in CSS, following the speculative CSS Extend Rules Specification. | https://github.com/csstools/postcss-extend-rule
     // require("postcss-advanced-variables"), // $variables, @if, @else, @for, @each, @mixin, @include, and @content | https://github.com/csstools/postcss-advanced-variables
     // require("postcss-preset-env")({
