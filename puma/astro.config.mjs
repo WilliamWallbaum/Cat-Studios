7c8f992f89a4f6c58d1a11038a78c98cca0b6ce7
Fuzzy-B <bill@catsimaginganddesign.com>
2023-08-01 15:37:28 -0600

    rebuilds project from unknown problem

--- puma/astro.config.mjs
+++ puma/astro.config.mjs
@@ -1,26 +1,4 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import image from "@astrojs/image";
-
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: "https://catstudios.online",
-  markdown: {
-    gfm: true
-  },
-  integrations: [mdx({
-    extendMarkdownConfig: false,
-    gfm: true
-  }), image(), react()],
-  compressHTML: false,
-  build: {
-    // Example: Generate `page.html` instead of `page/index.html` during build.
-    format: "file",
-    inlineStylesheets: `auto`
-  },
-  experimental: {
-    assets: true
-  }
-});
+export default defineConfig({});
