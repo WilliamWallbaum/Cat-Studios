5b7794a6f97291988f41b2294f69867798889cda
Fuzzy-B <bill@catsimaginganddesign.com>
2023-08-09 20:02:15 -0600

    adds resume styling

--- puma/src/scripts/dynamicToggler.js
+++ puma/src/scripts/dynamicToggler.js
@@ -1,35 +1,40 @@
+// dynamicToggler.js
 
-  // JavaScript code to handle button clicks and show/hide content elements
-const opacityChange = (
-  containerSelector,
-  activeContentSelector,
-  activeSubContentSelector
-) => {
-  const containerCol = document.querySelector(containerSelector);
-  const colContent = containerCol.querySelectorAll(".content, .sub-content");
+export function initializeDynamicToggler(col1Container, col2Container) {
+  const opacityChange = (
+    containerSelector,
+    activeContentSelector,
+    activeSubContentSelector
+  ) => {
+    const containerCol = document.querySelector(containerSelector);
+    const colContent = containerCol.querySelectorAll(".content, .sub-content");
 
-  colContent.forEach((container) => {
-    const isActiveContent =
-      container.id === activeContentSelector ||
-      container.id === activeSubContentSelector;
+    colContent.forEach((container) => {
+      const isActiveContent =
+        container.id === activeContentSelector ||
+        container.id === activeSubContentSelector;
 
-    container.classList.toggle("opacity-1", isActiveContent);
-    container.classList.toggle("opacity-0", !isActiveContent);
+      container.classList.toggle("opacity-1", isActiveContent);
+      container.classList.toggle("opacity-0", !isActiveContent);
+    });
+  };
+
+  const handleButtonClick = (event, containerSelector) => {
+    const targetButton = event.target.closest("button");
+
+    if (targetButton) {
+      const contentId = targetButton.getAttribute("data-content-id");
+      const subContentId = targetButton.getAttribute("data-sub-content-id");
+
+      opacityChange(containerSelector, contentId, subContentId || contentId);
+    }
+  };
+
+  col1Container.addEventListener("click", (event) => {
+    handleButtonClick(event, ".col-2");
   });
-};
 
-// Store references to the containers
-const col1Container = document.querySelector(".col-1");
-const col2Container = document.querySelector(".col-2");
-
-// Function to handle button clicks
-const handleButtonClick = (event, containerSelector) => {
-  const targetButton = event.target.closest("button");
-
-  if (targetButton) {
-    const contentId = targetButton.getAttribute("data-content-id");
-    const subContentId = targetButton.getAttribute("data-sub-content-id");
-
-    opacityChange(containerSelector, contentId, subContentId || contentId);
-  }
-};
+  col2Container.addEventListener("click", (event) => {
+    handleButtonClick(event, ".col-2");
+  });
+}
